7c3be081f45e182b7834f77fcf9c314b3899beb0
Jia Jun <damontham@hotmail.com>
2022-09-16 14:15:42 +0800

    final solution and updated README.md

---
+++ script.js
@@ -0,0 +1,30 @@
+const submitBtn = document.querySelector(".submit");
+const ratingContent = document.querySelector(".rating-content");
+const thankyouContent = document.querySelector(".thankyou");
+const ratingComponents = document.querySelectorAll(".rating-component");
+const selectedRating = document.querySelector(".selected-rating");
+let rating;
+
+submitBtn.addEventListener("click", onSubmit);
+
+ratingComponents.forEach((ratingComponent) => {
+  ratingComponent.addEventListener("click", ratingHandler);
+});
+
+function onSubmit() {
+  ratingContent.classList.add("hide");
+
+  selectedRating.innerHTML = rating;
+  thankyouContent.classList.remove("hide");
+  // console.log("Submit");
+}
+
+function ratingHandler(event) {
+  ratingComponents.forEach((ratingComponent) => {
+    ratingComponent.classList.remove("active");
+  });
+
+  event.target.classList.add("active");
+  rating = event.target.innerText;
+  // console.log("rating click");
+}
